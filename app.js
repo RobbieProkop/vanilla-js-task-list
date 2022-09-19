c78e442ccb1ff7339e3a3ccda21fe9532d9769b8
Robbie Prokop <robbieprokop@gmail.com>
2022-09-19 17:09:50 +0400

    add variables to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Define UI Variables
+
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.getElementById("filter");
+const taskInput = document.getElementById("task");
